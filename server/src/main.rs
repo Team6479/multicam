9f1d03c2f52dc3324accc2ef521eb9554f8f22a1
Leo Wilson <leo.wilson.xlw123@gmail.com>
2019-07-28 22:53:20 -0700

    Create a functioning TCP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,10 @@
 use rscam::{Camera, Config};
-use std::io::prelude::*;
-use std::fs::File;
+use std::io::Write;
+use std::net::TcpListener;
+use std::thread;
 
-fn main() {
-    let mut cam = Camera::new("/dev/video0").unwrap();
+fn start_cam(id: u8) {
+    let mut cam = Camera::new(&format!("/dev/video{}", id)).unwrap();
     cam.start(&Config {
         interval: (1, 5), // 30fps
         resolution: (1920, 1080),
@@ -11,8 +12,23 @@ fn main() {
         ..Default::default()
     }).unwrap();
 
-    let mut test = File::create("test.h264").unwrap();
-    loop {
-        test.write_all(&cam.capture().unwrap()[..]);
+    let listener = TcpListener::bind(&format!("0.0.0.0:580{}", id)).unwrap();
+    println!("Server started for {}", id);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                stream.write(&cam.capture().unwrap()[..]).unwrap();
+            },
+            Err(e) => println!("Error: {}", e),
+        }
     }
+    drop(listener); // close the server
+}
+
+fn main() {
+    let cam0 = thread::spawn(move || {
+        start_cam(0);
+        0
+    });
+    cam0.join();
 }
