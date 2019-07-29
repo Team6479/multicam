e3f70df85a3382c43c6de4a1cffaf21bd04c1661
Leo Wilson <leo.wilson.xlw123@gmail.com>
2019-07-28 21:16:28 -0700

    Remove unnecessary vector

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,9 +3,8 @@ use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
-    let mut cams = Vec::new();
-    cams.insert(0, Camera::new("/dev/video0").unwrap());
-    cams[0].start(&Config {
+    let mut cam = Camera::new("/dev/video0").unwrap();
+    cam.start(&Config {
         interval: (1, 5), // 30fps
         resolution: (1920, 1080),
         format: b"H264",
@@ -14,6 +13,6 @@ fn main() {
 
     let mut test = File::create("test.h264").unwrap();
     loop {
-        test.write_all(&cams[0].capture().unwrap()[..]);
+        test.write_all(&cam.capture().unwrap()[..]);
     }
 }
