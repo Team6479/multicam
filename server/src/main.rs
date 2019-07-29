6ba2b68cc66bac6f23a8b9c604fdb021ac81e9b6
Leo Wilson <leo.wilson.xlw123@gmail.com>
2019-07-28 21:07:43 -0700

    Add test recording functionality

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
+use rscam::{Camera, Config};
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cams = Vec::new();
+    cams.insert(0, Camera::new("/dev/video0").unwrap());
+    cams[0].start(&Config {
+        interval: (1, 5), // 30fps
+        resolution: (1920, 1080),
+        format: b"H264",
+        ..Default::default()
+    }).unwrap();
+
+    let mut test = File::create("test.h264").unwrap();
+    loop {
+        test.write_all(&cams[0].capture().unwrap()[..]);
+    }
 }
